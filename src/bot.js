f7cf9cf4f226765474cd7211a63b694674dea7f4
revanth-s <revanthvishal@gmail.com>
2020-10-07 16:41:56 +0530

    :lipstick: :zap: Improve console logs & '1' prompt commands
    General console log code clean-up & '!' prompt fixes.

--- src/bot.js
+++ src/bot.js
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv'
+import chalk from 'chalk'
 import discord from 'discord.js'
 
 dotenv.config()
@@ -6,98 +7,130 @@ dotenv.config()
 const client = new discord.Client()
 client.login(process.env.DISCORDJS_BOT_TOKEN)
 
+// change on immortal predators server
+const IMMORTAL_BOT_TEXT_CHANNEL_ID = '762756484928503848'
+const SERVER_ID = '762736585354510357'
+
 const PREFIX = '!'
+const log = console.log
 
 // Logging in the bot to discord
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.username}!`)
+  log(`Logged in as ${client.user.username}!`)
 })
 
 client.on('message', (message) => {
   // Prevents message debouncing
-  if (message.author.bot === true) return
+  if (message.author.bot) return
 
   // Checks for the availability of the server
   if (message.guild.available !== true) {
     message.channel
       .send(`Server unavailable at the moment!`)
       .then(() =>
-        console.log(
-          `Sent a server unavailable message to ${message.author.username}`
+        log(
+          `${chalk.bgYellow('Alert')} - Server unavailable sent to ${
+            message.author.username
+          }`
         )
       )
       .catch(console.error)
   } else {
     // Checks if the user is in right text channel
-    if (message.channel.id !== '762756484928503848') {
+    if (message.channel.id !== IMMORTAL_BOT_TEXT_CHANNEL_ID) {
       // Checks if the message starts with a '!' prompt in the wrong text channel
       if (message.content.charAt(0) === PREFIX) {
         message
           .reply(
             `Hey, Use this command on the ${message.guild.channels.cache
-              .get('762756484928503848')
+              .get(IMMORTAL_BOT_TEXT_CHANNEL_ID)
               .toString()} text channel!`
           )
           .then(() =>
-            console.log(
-              `Sent a channel redirection message to ${message.author.username}`
+            log(
+              `${chalk.bgBlue('Message')} - Redirect to text channel to ${
+                message.author.username
+              }`
             )
           )
           .catch(console.error)
       }
     } else {
-      // Only read commands with a '!' prompt
-      if (message.content.charAt(0) !== PREFIX) {
-        message
-          .reply(`Use '!' prompt to issue commands to me`)
-          .then(() =>
-            console.log(
-              `Sent a '!' prompt message to ${message.author.username}`
-            )
-          )
-          .catch(console.error)
-      } else {
-        // Splits the command from the prompt
-        const [command, ...args] = message.content
-          .substring(PREFIX.length)
-          .split(/\s+/)
+      // Splits the command from the prompt
+      const [command, ...args] = message.content.split(/\s+/)
 
+      // Checks if the command start with a '!' prompt
+      if (command.charAt(0) === PREFIX) {
         // Kick a user from the server
-        if (command === 'kick') {
+        if (command === '!kick') {
+          // Checks if author has permission to kick
+          if (!message.member.hasPermission('KICK_MEMBERS'))
+            return message
+              .reply(`You do not have permission to kick the user.`)
+              .then(() =>
+                log(
+                  `${chalk.bgRed(
+                    'Kick'
+                  )} - Author does not have permission to kick`
+                )
+              )
+              .catch(console.error)
+
+          // Checks if user has been mentioned
           if (args.length === 0)
             return message
               .reply(`Whom should I kick?`)
-              .then(() => console.log(`Kick - User not mentioned`))
+              .then(() => log(`${chalk.bgRed('Kick')} - User not mentioned`))
               .catch(console.error)
 
           const member = message.guild.members.cache.get(args[0])
           if (member) {
-            message.channel
-              .send(`${member.user.username} has been kicked`)
+            message
+              .reply(`${member.user.username} has been kicked.`)
               .then(() =>
-                console.log(`Kicked ${member.user.username} from the server`)
+                log(
+                  `${chalk.bgRed('Kick')} - ${
+                    member.user.username
+                  } has been kicked from the server`
+                )
               )
               .catch(console.error)
           } else {
-            message.channel
-              .send(`Invalid user name`)
-              .then(() => console.log(`Kick - Invalid username`))
+            message
+              .reply(`Invalid user name.`)
+              .then(() => log(`${chalk.bgRed('Kick')} - Invalid username`))
               .catch(console.error)
           }
         }
         // Mute a user for a given time
-        else if (command === 'mute') {
+        else if (command === '!mute') {
           if (args.length === 0)
             return message
               .reply(`Whom should I mute?`)
-              .then(() => console.log(`Mute - User not mentioned`))
+              .then(() => log(`${chalk.bgRed('Mute')} - User not mentioned`))
               .catch(console.error)
 
-          message.channel
-            .send(`<User> muted for <time>`)
-            .then(() => console.log(`Muted a user for <time>`))
+          message
+            .reply(`<User> muted for <time>`)
+            .then(() =>
+              log(
+                `${chalk.bgRed('Mute')} - ${
+                  member.user.username
+                } has been muted for <time>`
+              )
+            )
+            .catch(console.error)
+        } else {
+          return message
+            .reply(`Invalid command.`)
+            .then(() => log(`${chalk.bgYellow('Alert')} - Invalid command`))
             .catch(console.error)
         }
+      } else {
+        return message
+          .reply(`Use the '!' prompt to issue commands to me.`)
+          .then(() => log(`${chalk.bgYellow('Alert')} - '!' prompt required`))
+          .catch(console.error)
       }
     }
   }
