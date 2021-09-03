0369acb84322c1f0556875413a6eea65e74d25b4
Revanth <revanthvishal@gmail.com>
2021-09-03 20:07:40 +0530

    ⬆️🔨 Dependancy updates & code changes
    'kick' changed to 'disconnect' and fixes for breaking changes in discord.js v13.

--- src/bot.js
+++ src/bot.js
@@ -10,7 +10,26 @@ import discord from 'discord.js'
 
 dotenv.config()
 
-const client = new discord.Client()
+const client = new discord.Client({
+  intents: [
+    'GUILDS',
+    // 'GUILD_MEMBERS',
+    'GUILD_BANS',
+    'GUILD_EMOJIS_AND_STICKERS',
+    'GUILD_INTEGRATIONS',
+    // 'GUILD_WEBHOOKS',
+    'GUILD_INVITES',
+    'GUILD_VOICE_STATES',
+    // 'GUILD_PRESENCES',
+    'GUILD_MESSAGES',
+    'GUILD_MESSAGE_REACTIONS',
+    'GUILD_MESSAGE_TYPING',
+    'DIRECT_MESSAGES',
+    'DIRECT_MESSAGE_REACTIONS',
+    'DIRECT_MESSAGE_TYPING'
+  ]
+})
+
 // Logging in the bot to discord
 client.login(process.env.DISCORDJS_BOT_TOKEN)
 
@@ -22,28 +41,19 @@ client.on('ready', () => {
   log(`Logged in as ${chalk.blue(client.user.tag)}!`)
 
   // Set the presence of the bot
-  client.user
-    .setPresence({
-      activity: {
+  client.user.setPresence({
+    activities: [
+      {
         name: '!command',
         type: 'LISTENING'
-      },
-      status: 'online'
-    })
-    .then(() =>
-      log(
-        `${chalk.blue('Presence')} - ${
-          client.user.presence.status
-        } & ${client.user.presence.activities[0].type.toLowerCase()} to ${
-          client.user.presence.activities[0].name
-        }`
-      )
-    )
-    .catch(console.error)
+      }
+    ],
+    status: 'online'
+  })
 })
 
 // Event - checks if a message was sent in the server
-client.on('message', (message) => {
+client.on('messageCreate', (message) => {
   // Prevents message debouncing - Ignores messages from bot
   if (message.author.bot) return
 
@@ -88,7 +98,7 @@ client.on('message', (message) => {
         // Listing all commands
         return message
           .reply(
-            'Available commands are !kick, !mute, !unmute, !deafen, !undeafen'
+            'Available commands are !disconnect, !mute, !unmute, !deafen, !undeafen'
           )
           .then(() =>
             log(
@@ -100,36 +110,36 @@ client.on('message', (message) => {
             )
           )
           .catch(console.error)
-      } else if (command === '!kick') {
-        // Kick a member from an active voice channel
+      } else if (command === '!disconnect') {
+        // Disconnect a member from an active voice channel
 
-        // Checks if author has permission to kick
-        if (!message.member.hasPermission('ADMINISTRATOR')) {
+        // Checks if author has permission to disconnect
+        if (!message.member.permissions.has('ADMINISTRATOR')) {
           return message
             .reply(
-              'You do not have permissions to kick users. Request the admin for permissions'
+              'You do not have permissions to disconnect users. Request the admin for permissions'
             )
             .then(() =>
               log(
                 `${chalk.red(
-                  'Kick'
-                )} - You do not have permissions to kick - ${chalk.green(
+                  'Disconnect'
+                )} - You do not have permissions to disconnect - ${chalk.green(
                   message.author.tag
                 )} on ${chalk.cyan(message.guild.name)}`
               )
             )
             .catch(console.error)
-        } else if (!message.guild.me.hasPermission('ADMINISTRATOR')) {
-          // Checks if the bot has permissions to kick
+        } else if (!message.guild.me.permissions.has('ADMINISTRATOR')) {
+          // Checks if the bot has permissions to disconnect
           return message
             .reply(
-              'I do not have permissions to kick the user. You can enable permissions in the server settings.'
+              'I do not have permissions to disconnect the user. You can enable permissions in the server settings.'
             )
             .then(() =>
               log(
-                `${chalk.red('Kick')} - ${chalk.blue(
+                `${chalk.red('Disconnect')} - ${chalk.blue(
                   client.user.tag
-                )} does not have permissions to kick - ${chalk.green(
+                )} does not have permissions to disconnect - ${chalk.green(
                   message.author.tag
                 )} on ${chalk.cyan(message.guild.name)}`
               )
@@ -140,10 +150,12 @@ client.on('message', (message) => {
         // Checks if member has not been mentioned
         if (args.length === 0) {
           return message
-            .reply('Whom should I kick?')
+            .reply('Whom should I disconnect?')
             .then(() =>
               log(
-                `${chalk.red('Kick')} - User not mentioned - ${chalk.green(
+                `${chalk.red(
+                  'Disconnect'
+                )} - User not mentioned - ${chalk.green(
                   message.author.tag
                 )} on ${chalk.cyan(message.guild.name)}`
               )
@@ -154,23 +166,23 @@ client.on('message', (message) => {
         // Gets the user mentioned in the message
         const user = message.mentions.users.first()
         // Gets the member if they are available in the server
-        const member = message.guild.member(user)
+        const member = message.guild.members.cache.get(user.id)
         // Checks if the member is a valid user
         if (member) {
           // Checks if member is connected to a voice channel
           if (member.voice.channel) {
             return member.voice
-              .kick()
+              .disconnect()
               .then(() =>
                 message.channel.send(
-                  `${member} has been kicked from the voice channel!`
+                  `${member} has been disconnected from the voice channel!`
                 )
               )
               .then(() =>
                 log(
-                  `${chalk.red('Kick')} - ${chalk.magenta(
+                  `${chalk.red('Disconnect')} - ${chalk.magenta(
                     member.user.tag
-                  )} has been kicked - ${chalk.green(
+                  )} has been disconnected - ${chalk.green(
                     message.author.tag
                   )} on ${chalk.cyan(message.guild.name)}`
                 )
@@ -183,7 +195,7 @@ client.on('message', (message) => {
               )
               .then(() =>
                 log(
-                  `${chalk.red('Kick')} - ${chalk.magenta(
+                  `${chalk.red('Disconnect')} - ${chalk.magenta(
                     member.user.tag
                   )} is not connected to any voice channel - ${chalk.green(
                     message.author.tag
@@ -197,7 +209,7 @@ client.on('message', (message) => {
             .reply('Invalid user name.')
             .then(() =>
               log(
-                `${chalk.red('Kick')} - Invalid username - ${chalk.green(
+                `${chalk.red('Disconnect')} - Invalid username - ${chalk.green(
                   message.author.tag
                 )} on ${chalk.cyan(message.guild.name)}`
               )
@@ -208,7 +220,7 @@ client.on('message', (message) => {
         // Mute a user from all voice channel
 
         // Checks if author has permission to mute
-        if (!message.member.hasPermission('MUTE_MEMBERS')) {
+        if (!message.member.permissions.has('MUTE_MEMBERS')) {
           return message
             .reply(
               'You do not have permissions to mute users. Request the admin for permissions'
@@ -223,7 +235,7 @@ client.on('message', (message) => {
               )
             )
             .catch(console.error)
-        } else if (!message.guild.me.hasPermission('MUTE_MEMBERS')) {
+        } else if (!message.guild.me.permissions.has('MUTE_MEMBERS')) {
           // Checks if the bot has permissions to mute
           return message
             .reply(
@@ -257,7 +269,7 @@ client.on('message', (message) => {
         // Gets the user mentioned in the message
         const user = message.mentions.users.first()
         // Gets the member if they are available in the server
-        const member = message.guild.member(user)
+        const member = message.guild.members.cache.get(user.id)
         // Checks if the member is a valid user
         if (member) {
           // Checks if member is connected to a voice channel
@@ -327,7 +339,7 @@ client.on('message', (message) => {
         // Unmute a user from all voice channel
 
         // Checks if author has permission to unmute
-        if (!message.member.hasPermission('MUTE_MEMBERS')) {
+        if (!message.member.permissions.has('MUTE_MEMBERS')) {
           return message
             .reply(
               'You do not have permissions to unmute users. Request the admin for permissions'
@@ -342,7 +354,7 @@ client.on('message', (message) => {
               )
             )
             .catch(console.error)
-        } else if (!message.guild.me.hasPermission('MUTE_MEMBERS')) {
+        } else if (!message.guild.me.permissions.has('MUTE_MEMBERS')) {
           // Checks if the bot has permissions to unmute
           return message
             .reply(
@@ -376,7 +388,7 @@ client.on('message', (message) => {
         // Gets the user mentioned in the message
         const user = message.mentions.users.first()
         // Gets the member if they are available in the server
-        const member = message.guild.member(user)
+        const member = message.guild.members.cache.get(user.id)
         // Checks if the member is a valid user
         if (member) {
           // Checks if member is connected to a voice channel
@@ -446,7 +458,7 @@ client.on('message', (message) => {
         // Deafen a user from all voice channel
 
         // Checks if author has permission to deafen
-        if (!message.member.hasPermission('DEAFEN_MEMBERS')) {
+        if (!message.member.permissions.has('DEAFEN_MEMBERS')) {
           return message
             .reply(
               'You do not have permissions to deafen users. Request the admin for permissions'
@@ -461,7 +473,7 @@ client.on('message', (message) => {
               )
             )
             .catch(console.error)
-        } else if (!message.guild.me.hasPermission('DEAFEN_MEMBERS')) {
+        } else if (!message.guild.me.permissions.has('DEAFEN_MEMBERS')) {
           // Checks if the bot has permissions to deafen
           return message
             .reply(
@@ -495,7 +507,7 @@ client.on('message', (message) => {
         // Gets the user mentioned in the message
         const user = message.mentions.users.first()
         // Gets the member if they are available in the server
-        const member = message.guild.member(user)
+        const member = message.guild.members.cache.get(user.id)
         // Checks if the member is a valid user
         if (member) {
           // Checks if member is connected to a voice channel
@@ -565,7 +577,7 @@ client.on('message', (message) => {
         // Undeafen a user from all voice channel
 
         // Checks if author has permission to undeafen
-        if (!message.member.hasPermission('DEAFEN_MEMBERS')) {
+        if (!message.member.permissions.has('DEAFEN_MEMBERS')) {
           return message
             .reply(
               'You do not have permissions to undeafen users. Request the admin for permissions'
@@ -580,7 +592,7 @@ client.on('message', (message) => {
               )
             )
             .catch(console.error)
-        } else if (!message.guild.me.hasPermission('DEAFEN_MEMBERS')) {
+        } else if (!message.guild.me.permissions.has('DEAFEN_MEMBERS')) {
           // Checks if the bot has permissions to undeafen
           return message
             .reply(
@@ -614,7 +626,7 @@ client.on('message', (message) => {
         // Gets the user mentioned in the message
         const user = message.mentions.users.first()
         // Gets the member if they are available in the server
-        const member = message.guild.member(user)
+        const member = message.guild.members.cache.get(user.id)
         // Checks if the member is a valid user
         if (member) {
           // Checks if member is connected to a voice channel
@@ -683,7 +695,7 @@ client.on('message', (message) => {
       } else {
         return message
           .reply(
-            'Invalid command. Available commands are !kick, !mute, !unmute, !deafen or !undeafen.'
+            'Invalid command. Available commands are !disconnect, !mute, !unmute, !deafen or !undeafen.'
           )
           .then(() =>
             log(
